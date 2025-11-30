67dc1561d3c0ea957cbdb8ecc3767b233531b964
YousefElghanam <yousef.elghanam@gmail.com>
2025-12-01 00:02:13 +0100

    ADDED: how to clear eof: clearerr(stdin)

--- CPP_Module_00/ex01/main.cpp
+++ CPP_Module_00/ex01/main.cpp
@@ -1,18 +1,37 @@
 #include "PhoneBook.class.hpp"
 #include "Contact.class.hpp"
-#include <cstring>
-#include <istream>
-#include <ostream>
 #include <sstream>
-#include <string>
 #include <iostream>
+#include <cstdlib>
+#include <limits>
+#include <cstdio>
+
+static int	is_only_spaces(std::string str) {
+	for (std::string::iterator it = str.begin(); it != str.end(); it++) {
+		if (!isspace(*it))
+			return (0);
+	}
+	return (1);
+}
 
 static int	prompt_string_field(std::string prompt, std::string *field)
 {
+	std::string			s;
+	std::stringstream	ss;
+	
 	std::cout << prompt;
 	std::getline(std::cin, *field);
-	if (std::cin.eof())
-		return (std::cin.clear(), 1);
+	if (std::cin.eof() || field->empty() || is_only_spaces(*field)) {
+		if (std::cin.eof()) {
+			std::cout << std::endl;
+			std::clearerr(stdin);
+		}
+		std::cin.clear();
+		// std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		return (1);
+	}
+	if (std::cin.fail())
+		exit(1);
 	return (0);
 }
 
@@ -25,7 +44,7 @@ static int	prompt_new_contact(Contact *contact)
 		|| prompt_string_field("Phone Number: ", &contact->phone)
 		|| prompt_string_field("Darkest Secret: ", &contact->secret))
 		{
-			std::cout << std::endl << "Adding new contact failed. Can't have an empty field" << std::endl;
+			std::cout << "Adding new contact failed. Can't have an empty field" << std::endl;
 			return (1);
 		}
 	return (0);
@@ -38,9 +57,22 @@ static int	prompt_contact_id(size_t *id)
 
 	std::cout << "Enter contact id: ";
 	std::getline(std::cin, s);
-	if (std::cin.eof())
-		return (std::cin.clear(), 1);
+	if (std::cin.eof() || s.empty() || is_only_spaces(s)) {
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		if (std::cin.eof())
+			std::cout << std::endl;
+		std::cout << "Search failed. Empty input" << std::endl;
+		return (1);
+	}
+	if (std::cin.fail()) {
+		exit(1);
+	}
 	ss >> *id;
+	if (ss.fail()) {
+		std::cout << "Invalid id. Enter a number" << std::endl;
+		return (1);
+	}
 	return (0);
 }
 
@@ -52,12 +84,20 @@ int	main(void) {
 	std::cout << "===PHONEBOOK===" << std::endl;
 	std::cout << "ADD, SEARCH, EXIT" << std::endl;
 	while (1) {
-		std::cin.clear();
-		std::cout  << std::endl << ">";
-		std::getline(std::cin, input).fail();
+		std::cout  << ">";
+		std::getline(std::cin, input);
 		if (std::cin.eof() || input == "EXIT") {
 			std::cout << std::endl << "Exiting" << std::endl;
 			return (0);
+		}
+		else if (input.empty() || is_only_spaces(input)) {
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cin.clear();
+			continue ;
+		}
+		else if (!std::cin) {
+			std::cout << "Unexpected input, Exiting" << std::endl;
+			return (1);
 		}
 
 		else if (input == "ADD") {
@@ -73,10 +113,10 @@ int	main(void) {
 			size_t	id;
 			Contact	contact;
 
-			if (phone_book.size == 0) {
-				std::cout << "The PhoneBook is empty" << std::endl;
-				continue ;
-			}
+			// if (phone_book.size == 0) {
+			// 	std::cout << "The PhoneBook is empty" << std::endl;
+			// 	continue ;
+			// }
 			phone_book.display_all_contacts();
 			if (prompt_contact_id(&id))
 				continue ;
