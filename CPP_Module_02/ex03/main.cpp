aa5f106e6f66273872f05dfd9a562563ebd03f82
YousefElghanam <yousef.elghanam@gmail.com>
2026-01-14 12:42:06 +0100

    CPP02: ready to submit

--- CPP_Module_02/ex03/main.cpp
+++ CPP_Module_02/ex03/main.cpp
@@ -14,9 +14,8 @@ void	print_binary(int num) {
 }
 
 void	test1(void) {
-	Point	a(0, 0);
-	Point	b(2, 0);
-	Point	c(0, 2);
+	std::cout << "Simple (0,0) (2,0) (0,2) triangle" << std::endl;
+	Point	a(0, 0), b(2, 0), c(0, 2);
 	std::cout << "(.5, .5)\t" << bsp(a, b, c, Point(0.5f, 0.5f)) << std::endl;	// inside
 	std::cout << "(2, 2)\t\t" << bsp(a, b, c, Point(2, 2)) << std::endl;			// outside
 	std::cout << "(1, 0)\t\t" << bsp(a, b, c, Point(1, 0)) << std::endl;	// outside
@@ -25,35 +24,22 @@ void	test1(void) {
 	std::cout << "(0, 0)\t\t" << bsp(a, b, c, Point(0, 0)) << std::endl;	// outside
 }
 
+void	test2(void) {
+	Point	a(1, 1), b(-1, 1), c(0, -1);
+	TEST("Inside simple", bsp(a, b, c, Point(0, 0)));
+	TEST("On edge", bsp(a, b, c, Point(0, 1)));
+	TEST("On vertex", bsp(a, b, c, Point(1, 1)));
+	TEST("Outside", bsp(a, b, c, Point(2, 2)));
+}
+
 int	main(void) {
-	// Fixed	neg(-10);
-	// std::cout << neg << std::endl;
-	Point	a(-1, -1);
-	Point	b( 1, -1);
-	Point	c( 0,  1);
-
-	// Point	a(0, 0);
-	// Point	b(0, 2);
-	// Point	c(2, 0); // reversed order
-
-	// Point	a(0, 0);
-	// Point	b(1, 0);
-	// Point	c(0, 1);
-
-	// Point	a(10000, 0);
-	// Point	b(0, 10000);
-	// Point	c(0, 0);
-
-	// Point	a(0, 0);
-	// Point	b(1, 1);
-	// Point	c(2, 2); // collinear
-	// Point	point(0.1f, 0.1f);
-	std::cout << "Simple (0,0) (2,0) (0,2) triangle" << std::endl;
+	// Point	a(-1, -1), b( 1, -1), c( 0,  1);
+	// Point	a(0, 0), b(0, 2), c(2, 0); // reversed order
+	// Point	a(0, 0), b(1, 0), c(0, 1);
+	// Point	a(10000, 0), b(0, 10000), c(0, 0);
+	// Point	a(0, 0), b(1, 1), c(2, 2); // collinear
 	test1();
-	// std::cout << "===============================" << std::endl;
-	// TEST("Inside simple", bsp(a, b, c, Point(0.5f, 0.5f)));
-	// TEST("On edge", bsp(a, b, c, Point(1, 0)));
-	// TEST("On vertex", bsp(a, b, c, Point(0, 0)));
-	// TEST("Outside", bsp(a, b, c, Point(2, 2)));
+	std::cout << "===============================" << std::endl;
+	test2();
 	return 0;
 }
