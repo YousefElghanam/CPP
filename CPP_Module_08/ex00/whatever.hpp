d67b748b355c822464279a2704fc4a361aa4716c
YousefElghanam <yousef.elghanam@gmail.com>
2026-02-06 19:40:45 +0100

    ADD: cpp08

---
+++ CPP_Module_08/ex00/whatever.hpp
@@ -0,0 +1,11 @@
+#ifndef EASY_TEMPLATE_HPP
+# define EASY_TEMPLATE_HPP
+
+# include <
+
+template <typename T, int>
+	int	easyfind(T container, int num) {
+		for ()
+	}
+
+#endif /* EASY_TEMPLATE_HPP */
