62e059d4bc9c1dd1087097dbe0c3b1133c24fb17
YousefElghanam <yousef.elghanam@gmail.com>
2026-02-16 15:59:46 +0100

    what

--- CPP_Module_09/ex00/BitcoinExchange.hpp
+++ CPP_Module_09/ex00/BitcoinExchange.hpp
@@ -8,7 +8,7 @@ class BitcoinExchange {
 		BitcoinExchange(void);
 		BitcoinExchange(const BitcoinExchange& obj);
 		BitcoinExchange&	operator=(const BitcoinExchange& obj);
-		
+
 	public:
 		virtual ~BitcoinExchange(void) = 0;
 		static int	exchange(std::ifstream& data, std::ifstream& input);
