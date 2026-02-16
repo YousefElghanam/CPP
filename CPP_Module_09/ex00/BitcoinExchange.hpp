2142fc07df52f5b1cb951d4265c73207c1106a4e
YousefElghanam <yousef.elghanam@gmail.com>
2026-02-16 15:55:15 +0100

    ADD: basic data.csv  parsing

--- CPP_Module_09/ex00/BitcoinExchange.hpp
+++ CPP_Module_09/ex00/BitcoinExchange.hpp
@@ -2,13 +2,26 @@
 # define BITCOIN_EXCHANGE_CLASS_HPP
 
 # include <fstream>
+# include <map>
+
+typedef struct s_date {
+	int	year;
+	int	month;
+	int	day;
+}	t_date;
+
+struct f_less_date {
+	bool	operator()(t_date& d1, t_date& d2) const;
+};
 
 class BitcoinExchange {
 	private:
+		std::map<t_date, double>	data;
+
 		BitcoinExchange(void);
 		BitcoinExchange(const BitcoinExchange& obj);
 		BitcoinExchange&	operator=(const BitcoinExchange& obj);
-		
+
 	public:
 		virtual ~BitcoinExchange(void) = 0;
 		static int	exchange(std::ifstream& data, std::ifstream& input);
