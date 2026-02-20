7cd27c616c60eb5995112074a88619cd9bf32e3e
YousefElghanam <yousef.elghanam@gmail.com>
2026-02-20 23:01:06 +0100

    IN PROGRESS cpp09-ex02

--- CPP_Module_09/ex02/main.cpp
+++ CPP_Module_09/ex02/main.cpp
@@ -1,11 +1,10 @@
 #include "PmergeMe.hpp"
 #include <iostream>
-#include <fstream>
 
 int	main(int argc, char** argv) {
 	if (argc != 2) {
 		std::cerr << "Takes only exactly two arguments" << std::endl;
 		return 1;
 	}
-	return RPN::calculate(argv[1]);
+	return PmergeMe::calculate(argv[1]);
 }
